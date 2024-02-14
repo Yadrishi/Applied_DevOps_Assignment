31a4c9fde5fd91abfd05b94a3efe1cb740c9611f
Yadrishi <dixityadrishi@gmail.com>
2024-02-15 00:02:24 +0530

    one added

---
+++ one.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main{
+int a;
+scanf("%d" . &a);
+if (a%4==0) {
+    printf("the year is a leap year");
+}
+else if (a%100=0) {
+printf("the year is not a leap year");
+}
+
+else if (a%400==0) {
+    printf("the year is a leap year");
+}
+
+else if (a%3200==0) {
+    printf("the year is not a leap year");
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+//     int a;
+//     char c;
+
+//     scanf("%d ", &a);
+//     scanf("%c", &c);
+
+//     printf("%d\n",a);
+//     printf("%c\n",c);
+
+// return 0;
+// }
